06c47c3fc89d886d51acc2df8819cb122a0b1f3b
Sarah <sarorra0@gmail.com>
2022-01-19 21:43:30 -0800

    Initialized React frontend

--- db/seeds.js
+++ db/seeds.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
-const connectDB = require('./')
+const connectDB = require('.')
 const User = require("../models/User");
 const Card = require("../models/Card")
 const Deck = require("../models/Deck");
 const Folder = require('../models/Folder')
 
 
-connectDB(process.env.MONGO_URI)
 
 const seedDB = async () => {
     // await User.deleteMany({})
@@ -17,22 +16,15 @@ const seedDB = async () => {
     // const user = new User({ username: "sarah", email: "sarah123@gmail.com", password: "heythere" }).save()
     const folder = new Folder({title: "some folder", owner: mongoose.Types.ObjectId("61dde20542d764665d5447e2"), decks: ["61dde20542d764665d5447e4", "61dde20542d764665d5447e3"]}).save()
 
-    
-    // const cards = [
-    //     { owner: user._id, title: "one", private: false },
-    //     { owner: user._id, title: "two" , private: true},
-    //     { owner: user._id, title: "one three" },
-    //     { owner: user._id, title: "f one our" },
-    //     { owner: user._id, title: "fioneve" },
-    // ]
-
     // await Deck.insertMany(cards)
     const deck = await Deck.find({})
-    console.log(deck);
-
     }
-    
-
+ 
+  
+connectDB(process.env.MONGO_URI)
+    .then(() => console.log('db connected!'))
+    .catch(err => console.log(err))
+  
 seedDB()
     .then(() => {
     mongoose.connection.close();
