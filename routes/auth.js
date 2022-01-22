abcc06c719bfab34025e21ad53f15aeeea5d7964
Sarah <sarorra0@gmail.com>
2022-01-21 19:38:00 -0800

    Fixed arguments to validatePost mw

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { register, login } = require('../controllers/auth')
 const { validatePost } = require('../middleware/validators')
 
-router.post('/register', validatePost('users'), register)
-router.post('/login', validatePost('users'), login)
+router.post('/register', validatePost('register'), register)
+router.post('/login', validatePost('login'), login)
 
 module.exports = router;
